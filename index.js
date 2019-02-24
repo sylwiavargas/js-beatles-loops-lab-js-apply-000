7c76e880284b162bafebd4cc4209f5e566d52dc2
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-02-24 01:59:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians,instruments) {
   const array = [ ];
   for (let i=0, l=musicians.length; i < l; i++) {
-  array.push('{musicians[i]} plays {instruments[i]}');
+  array.push('${musicians[i]} plays ${instruments[i]}');
   }
   return array;
 }
