97564a0e67f63f54f0c27ddf27fb1f54e882bae6
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-02-24 01:50:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function theBeatlesPlay(musicians,instruments) {
   return array;
 }
 
-function johnLennonFacts {
+function johnLennonFacts() {
+  const theFacts = ["He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"];
   
 }
